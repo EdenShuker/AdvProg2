93591b2e01ec39f22ffb93019033a1394d01b069
Tamir <Tamir@DESKTOP-8R67EMV>
2017-04-02 18:29:43 +0300

    suppppp

---
+++ SearchAlgorithmsLib/Searcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher<T> : ISearcher<T>
+    {
+        private SimplePriorityQueue<State<T>> openList;
+        private int evaluatedNodes;
+        // a property of openList 
+        public int OpenListSize
+        {
+            get { return openList.Count; }
+        }
+
+        public Searcher()
+        {
+            openList = new SimplePriorityQueue<State<T>>();
+            evaluatedNodes = 0;
+        }
+        protected State<T> popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.Dequeue();
+        }
+
+        // ISearcher’s methods:
+        public int getNumberOfNodesEvaluated() { return evaluatedNodes; }
+        public abstract Solution search(ISearchable<T> searchable);
+    }
+}
