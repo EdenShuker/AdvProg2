cdca2804a2bcfce78544d10786e5363b16ae8d23
Tamir <Tamir@DESKTOP-8R67EMV>
2017-04-02 19:02:51 +0300

    jlkjlkj

--- SearchAlgorithmsLib/ISearchable .cs
+++ SearchAlgorithmsLib/ISearchable .cs
@@ -9,7 +9,12 @@ namespace SearchAlgorithmsLib
     public interface ISearchable<T>
     {
         State<T> getInitialState();
+
         State<T> getGoalState();
+
+
+
+
         List<State<T>> getAllPossibleStates(State<T> s);
     }
 }
