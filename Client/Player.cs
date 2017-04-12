c9e9a14e056e7987b069d7b90b25eed31546f96e
Tamir <Tamir@DESKTOP-8R67EMV>
2017-04-12 11:14:11 +0300

    player

--- Client/Player.cs
+++ Client/Player.cs
@@ -51,6 +51,7 @@ namespace Client
                         {
                             this.isConnected = false;
                         }
+
                     }
                 }
             }).Start();
